077f7cc1547f3a48306e8d6453db6520e3fe0bfc
TRAN TIEN DAT <64526462+songdeyeu@users.noreply.github.com>
2021-07-11 00:51:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à, chưa ngủ hả!',
+    introTitle: 'Xin chào, bây giờ mới chịu dậy hà!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao ai cứ đi nhầm đường
+    Lạc vào tim ai lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng aii đã say ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng aii đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích aiii <33',
+    btnNo: 'Không, àggg :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao ... <3',
+    btnReply: 'Gửi cho tuiii <3',
+    reply: 'Ấy thì ấy mà không ấy thì ấy <33333333',
+    mess: 'Tui biết mà 🥰. Chúc một ngày làm việc hiệu quả 😘😘',
+    messDesc: 'Tối nay 7h tui qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
